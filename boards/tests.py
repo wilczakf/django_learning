42ab24d8bf994d663acb03dbf000f5870faa8434
Filip Wilczak <majinvlq@gmail.com>
2022-11-26 23:07:49 +0100

    continuation of adding posts creation, relevant tests etc.

--- boards/tests.py
+++ boards/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from django.contrib.auth.models import User
 from boards.views import home, board_topics, new_topic
-from boards.models import Board
+from boards.models import Board, Topic, Post
 
 
 class HomeTests(TestCase):
@@ -12,14 +13,14 @@ class HomeTests(TestCase):
         url = reverse("home")
         self.response = self.client.get(url)
 
-    def test_home_view_status_code(self):
+    def test__home_view_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
-    def test_home_url_resolves_home_view(self):
+    def test__home_url_resolves_home_view(self):
         view = resolve("/")
         self.assertEqual(view.func, home)
 
-    def test_home_view_contains_link_to_topics_page(self):
+    def test__home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse("board_topics", kwargs={"board_pk": self.board.pk})
         self.assertContains(self.response, f'href="{board_topics_url}"')
 
@@ -31,48 +32,92 @@ class BoardTopicsTests(TestCase):
     def get_url(self, url: str):
         return self.client.get(url)
 
-    def test_board_topics_view_success_status_code(self):
+    def test__board_topics_view_success_status_code(self):
         url = reverse("board_topics", kwargs={"board_pk": 1})
         response = self.get_url(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_board_topics_view_not_found_status_code(self):
+    def test__board_topics_view_not_found_status_code(self):
         url = reverse("board_topics", kwargs={"board_pk": 99})
         response = self.get_url(url)
         self.assertEqual(response.status_code, 404)
 
-    def test_board_topics_url_resolves_board_topics_view(self):
+    def test__board_topics_url_resolves_board_topics_view(self):
         view = resolve("/boards/1/")
         self.assertEqual(view.func, board_topics)
 
-    def test_board_topics_view_contains_link_to_homepage(self):
-        board_topics_url = reverse("board_topics", kwargs={"board_pk": 1})
+    def test__board_topics_view_contains_nav_links(self):
+        board_pk_kwarg = {"board_pk": 1}
+        board_topics_url = reverse("board_topics", kwargs=board_pk_kwarg)
+        home_url = reverse("home")
+        new_topic_url = reverse("new_topic", kwargs=board_pk_kwarg)
+
         response = self.client.get(board_topics_url)
-        homepage_url = reverse("home")
-        self.assertContains(response, f'href="{homepage_url}"')
+
+        self.assertContains(response, f'href="{home_url}"')
+        self.assertContains(response, f'href="{new_topic_url}"')
 
 
 class NewTopicTests(TestCase):
     def setUp(self) -> None:
         Board.objects.create(name="Heheszki", description="All the heheszki.")
+        User.objects.create(username="Walczak", email="walczak@ls.com", password="123")
 
-    def test_new_topic_success_status_code(self):
+    def test__new_topic_success_status_code(self):
         url = reverse("new_topic", kwargs={"board_pk": 1})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_new_topic_not_found_status_code(self):
+    def test__new_topic_not_found_status_code(self):
         url = reverse("new_topic", kwargs={"board_pk": 99})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
-    def test_new_topic_resolves_new_topic_view(self):
+    def test__new_topic_resolves_new_topic_view(self):
         view = resolve("/boards/1/new/")
         self.assertEqual(view.func, new_topic)
 
-    def test_new_topic_view_contains_link_back_to_board_topics_view(self):
+    def test__new_topic_view_contains_link_back_to_board_topics_view(self):
         board_pk_kwarg = {"board_pk": 1}
         new_topic_url = reverse("new_topic", kwargs=board_pk_kwarg)
         board_topics_url = reverse("board_topics", kwargs=board_pk_kwarg)
         response = self.client.get(new_topic_url)
         self.assertContains(response, f'href="{board_topics_url}"')
+
+    def test__csrf(self):
+        url = reverse("new_topic", kwargs={"board_pk": 1})
+        response = self.client.get(url)
+        self.assertContains(response, "csrfmiddlewaretoken")
+
+    def test__new_topic_valid_post_data(self):
+        url = reverse("new_topic", kwargs={"board_pk": 1})
+        data = {"subject": "Shitstorm", "message": "You suck."}
+
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test__new_topic_invalid_post_data(self):
+        """
+        Invalid post data should not redirect.
+        The expected behaviour is to show the form again with validation errors.
+        """
+
+        url = reverse("new_topic", kwargs={"board_pk": 1})
+        response = self.client.post(url, {})
+        self.assertEqual(response.status_code, 200)
+
+
+    def test__new_topic_invalid_post_data_empty_fields(self):
+        """
+        Invalid post data should not redirect.
+        The expected behaviour is to show the form again with validation errors.
+        """
+
+        url = reverse("new_topic", kwargs={"board_pk": 1})
+        data = {"subject": "", "message": ""}
+
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from boards.models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from boards.models import Board, Topic, Post
 
 
 def home(request):
@@ -14,5 +15,19 @@ def board_topics(request, board_pk):
 
 def new_topic(request, board_pk):
     board = get_object_or_404(Board, pk=board_pk)
-    return render(request, "new_topic.html", {"board": board})
 
+    if request.method == "POST":
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+
+        user = User.objects.first()  # todo - get currently logged user
+
+        topic = Topic.objects.create(subject=subject, board=board, starting_user=user)
+
+        post = Post.objects.create(message=message, topic=topic, created_by=user)
+
+        return redirect(
+            "board_topics", board_pk=board.pk
+        )  # todo redirect to created topic page
+
+    return render(request, "new_topic.html", {"board": board})
