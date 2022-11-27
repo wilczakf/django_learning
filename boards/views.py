9aa0978afbd98fbbd98ab16f07c3c981d6570cf4
Filip Wilczak <88107121+wilczakf@users.noreply.github.com>
2022-11-27 19:41:45 +0100

    Implement forms (#1)
    * topic form implementation part 1
    
    * continuation of adding posts creation, relevant tests etc.
    
    * implemented usage of django forms
    
    * refactored adding new post using django widget tweaks; implemented forms template for includes

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from boards.models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from boards.forms import NewTopicForm
+from boards.models import Board, Topic, Post
 
 
 def home(request):
@@ -10,3 +12,31 @@ def home(request):
 def board_topics(request, board_pk):
     board = get_object_or_404(Board, pk=board_pk)
     return render(request, "topics.html", {"board": board})
+
+
+def new_topic(request, board_pk):
+    board = get_object_or_404(Board, pk=board_pk)
+    user = User.objects.first()  # todo get currently logged user
+
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starting_user = user
+            topic.save()
+
+            post = Post.objects.create(
+                message=form.cleaned_data.get("message"),
+                topic=topic,
+                created_by=user
+            )
+
+            return redirect(
+                "board_topics", board_pk=board.pk
+            )  # todo redirect to created topic page
+
+    else:
+        form = NewTopicForm()
+
+    return render(request, "new_topic.html", {"board": board, "form": form})
